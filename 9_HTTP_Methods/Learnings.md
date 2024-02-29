d2a65c9163051c9fb0f115870b5771445d34c9bc
unstoppableayush <ayushkum884@gmail.com>
2024-02-29 22:35:29 +0530

    http methods done

---
+++ 9_HTTP_Methods/Learnings.md
@@ -0,0 +1,10 @@
+# HTTP Methods
+```
+    GET -> When you want to get some data from the server.
+
+    POST -> When you want to send and mutate some data in server.
+
+    PUT  
+    PATCH
+    DELETE
+```
---
+++ 9_HTTP_Methods/index.js
@@ -0,0 +1,63 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const myServer = http.createServer((req , res)=>{
+
+    if(req.url === '/favicon.ico') return res.end();
+
+
+    const log = `${Date.now()} : ${req.url} : New Request Recieved \n`;
+    const myUrl = url.parse(req.url , true);
+    console.log(myUrl);
+
+    fs.appendFile('log.txt' , log , (err , data)=>{
+
+        switch(myUrl.pathname){
+
+            case '/' : 
+
+            if(req.method == 'GET') res.end("Home Page");
+            break;
+
+            case '/about' : 
+            //query parameter
+            const username = myUrl.query.myname ;
+            console.log(username)
+            res.end(`Hii,  ${username}`);
+            break;
+
+            case "/search":
+                const search = myUrl.query.search_query;
+                console.log(search)
+                res.end("Here are your result for "+search);
+                break;
+
+            case '/signup':
+
+                if(req.method == 'GET') res.end("This is a signup From");
+                else if(req.method == "POST"){
+                    //DB Query
+                    res.end("Success");
+                }
+                break;
+            default:
+                res.end("404 Not Found")
+
+        }
+        
+        
+    })
+
+
+    // console.log(req.headers)
+    // console.log(req)
+    
+
+});
+
+const PORT = 8000;
+myServer.listen(8000, ()=>{
+    console.log(`Server started at port no ${PORT} `);
+})
+
